6b46cb61f838ec604c392899b306d96a05340b11
Daniella Anokwu <anokwudaniella@gmail.com>
2025-07-28 21:00:56 +0100

    Initial setup with Vite + Homepage update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Campus Companion 👩🏽‍🎓📚</h1>
+      <p>Your all-in-one tool for budgeting, organizing, and thriving in school life.</p>
+    </div>
+  );
+}
+
+export default App;
